fb097c1a499c788f393927d8051087820edad29c
Sunny Pukkalli <pukkallisunny@gmail.com>
2020-05-22 19:40:44 +0530

    removing hystrix

--- src/main/java/com/ms/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/ms/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,17 +2,17 @@ package com.ms.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
-@EnableHystrixDashboard
+//@EnableCircuitBreaker
+//@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	@Bean
--- src/main/java/com/ms/moviecatalogservice/service/UserRatingService.java
+++ src/main/java/com/ms/moviecatalogservice/service/UserRatingService.java
@@ -2,7 +2,7 @@ package com.ms.moviecatalogservice.service;
 
 import com.ms.moviecatalogservice.model.Rating;
 import com.ms.moviecatalogservice.model.UserRating;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +17,12 @@ public class UserRatingService {
     private RestTemplate restTemplate;
 
 
-    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+//    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
     public UserRating getUserRating(@PathVariable("userId") String userId) {
         return restTemplate.getForObject("http://ratings-data/ratings/users/"+userId, UserRating.class);
     }
 
+/*
     private UserRating getFallbackUserRating(@PathVariable("userId") String userId) {
         UserRating rating = new UserRating();
         rating.setRatings(
@@ -32,5 +33,6 @@ public class UserRatingService {
 
         return rating;
     }
+*/
 
 }
