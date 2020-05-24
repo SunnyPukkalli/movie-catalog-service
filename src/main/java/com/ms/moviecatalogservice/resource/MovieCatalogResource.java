232013e2850895b702956db5fa00e6219fb1eefe
Sunny Pukkalli <pukkallisunny@gmail.com>
2020-05-24 19:49:48 +0530

    Removing Eureka Code and changing version to test in pom.xml

--- src/main/java/com/ms/moviecatalogservice/resource/MovieCatalogResource.java
+++ src/main/java/com/ms/moviecatalogservice/resource/MovieCatalogResource.java
@@ -2,17 +2,13 @@ package com.ms.moviecatalogservice.resource;
 
 
 import com.ms.moviecatalogservice.model.CatalogItem;
-import com.ms.moviecatalogservice.model.Movie;
-import com.ms.moviecatalogservice.model.Rating;
 import com.ms.moviecatalogservice.model.UserRating;
 import com.ms.moviecatalogservice.service.MovieInfoService;
 import com.ms.moviecatalogservice.service.UserRatingService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,38 +34,4 @@ public class MovieCatalogResource {
                 .map(rating -> movieInfoService.getCatalogItem(rating))
                 .collect(Collectors.toList());
     }
-
 }
-
-
-/*
-    @Autowired
-    private RestTemplate restTemplate;
-
-    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId){
-        return Arrays.asList(new CatalogItem("No Movie","",0));
-    }
-
-    @RequestMapping("{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
-        // Get all rated movie ID's
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1",1),
-                new Rating("2",2),
-                new Rating("3",3)
-        );
-
-        // Get movie details for each movie ID
-        return ratings.stream().map(rating -> {
-           Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getName(),movie.getDesc(),rating.getRating());
-        })
-        .collect(Collectors.toList());
-
-        // Put them all together
-
-        // return Collections.singletonList(new CatalogItem("ThunderStorm","Test Description",4));
-    }
-
-    */
--- src/main/java/com/ms/moviecatalogservice/service/MovieInfoService.java
+++ src/main/java/com/ms/moviecatalogservice/service/MovieInfoService.java
@@ -3,7 +3,6 @@ package com.ms.moviecatalogservice.service;
 import com.ms.moviecatalogservice.model.CatalogItem;
 import com.ms.moviecatalogservice.model.Movie;
 import com.ms.moviecatalogservice.model.Rating;
-//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,17 +16,9 @@ public class MovieInfoService {
     private RestTemplate restTemplate;
 
 
-//    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
     public CatalogItem getCatalogItem(Rating rating) {
-        Movie movie = restTemplate.getForObject("http://movie-info/movies/" + rating.getMovieId(), Movie.class);
+        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getName(), movie.getDesc(), rating.getRating());
     }
-/*
-
-    private CatalogItem getFallbackCatalogItem(Rating rating) {
-        return new CatalogItem("Movie Not Found","",rating.getRating());
-    }
-
-*/
 
 }
