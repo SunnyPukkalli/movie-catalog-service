33e00278fde4d021318c9d483045c946006de537
Sunny Pukkalli <pukkallisunny@gmail.com>
2020-06-10 00:09:07 +0530

    Updating env variables

--- src/main/java/com/ms/moviecatalogservice/service/MovieInfoService.java
+++ src/main/java/com/ms/moviecatalogservice/service/MovieInfoService.java
@@ -15,7 +15,7 @@ public class MovieInfoService {
 
     Logger logger = LoggerFactory.getLogger(MovieInfoService.class);
 
-    @Value("${movie.info:default}")
+    @Value("${movie-info:default}")
     private String MOVIE_INFO_URL;
 
     @Autowired
